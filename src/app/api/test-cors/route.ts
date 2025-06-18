55a2fb1891dd37ff439aba2bb92a15ab70165d2d
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-06-18 14:17:48 -0500

    Commit with type error

--- src/app/api/test-cors/route.ts
+++ src/app/api/test-cors/route.ts
@@ -7,6 +7,7 @@ const corsHeaders = new Headers({
 });
 
 export async function POST() {
+  const a: string = 2;
   return new NextResponse(JSON.stringify({ message: "Success" }), {
     headers: corsHeaders,
     status: 200,
